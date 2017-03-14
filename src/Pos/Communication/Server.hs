04ecc89ea8691256b2a0cca2662b53f544c85ec8
Chris Done <chrisdone@gmail.com>
2017-03-14 16:55:42 +0000

    Copied from upstream

---
+++ src/Pos/Communication/Server.hs
@@ -0,0 +1,61 @@
+-- | Server part.
+
+module Pos.Communication.Server
+       ( allListeners
+       , allStubListeners
+       , serverLoggerName
+       , module Pos.Communication.Server.SysStart
+       ) where
+
+import           Data.Tagged                       (Tagged, proxy, unproxy, untag)
+import           System.Wlog                       (LoggerName, WithLogger)
+import           Universum
+
+import           Pos.Binary.Communication          ()
+import           Pos.Block.Network.Listeners       (blockListeners, blockStubListeners)
+import           Pos.Communication.Protocol        (ListenerSpec (..), OutSpecs)
+import           Pos.Communication.Server.SysStart
+import           Pos.Communication.Util            (wrapListener)
+import           Pos.Delegation.Listeners          (delegationListeners,
+                                                    delegationStubListeners)
+import           Pos.Ssc.Class                     (SscHelpersClass (..),
+                                                    SscListenersClass (..),
+                                                    SscWorkersClass)
+import           Pos.Txp                           (txListeners, txStubListeners)
+import           Pos.Update                        (usListeners, usStubListeners)
+import           Pos.Util                          (mconcatPair)
+import           Pos.WorkMode                      (WorkMode)
+
+-- | All listeners running on one node.
+allListeners
+    :: (SscListenersClass ssc, SscWorkersClass ssc, WorkMode ssc m)
+    => m ([ListenerSpec m], OutSpecs)
+allListeners = mconcatPair <$> sequence
+        [ modifier "block"      <$> blockListeners
+        , modifier "ssc"        <$> untag <$> sscListeners
+        , modifier "tx"         <$> txListeners
+        , modifier "delegation" <$> pure delegationListeners
+        , modifier "update"     <$> usListeners
+        ]
+  where
+    modifier lname = over _1 (map pModifier)
+      where
+        pModifier (ListenerSpec h spec) =
+            ListenerSpec (\vI -> wrapListener (serverLoggerName <> lname) $ h vI) spec
+
+-- | All listeners running on one node.
+allStubListeners
+    :: (SscListenersClass ssc, WithLogger m, SscHelpersClass ssc)
+    => Tagged ssc ([ListenerSpec m], OutSpecs)
+allStubListeners = unproxy $ \sscProxy ->
+    mconcatPair
+        [ proxy blockStubListeners sscProxy
+        , proxy sscStubListeners sscProxy
+        , txStubListeners
+        , delegationStubListeners
+        , usStubListeners
+        ]
+
+-- | Logger name for server.
+serverLoggerName :: LoggerName
+serverLoggerName = "server"
---
+++ src/Pos/Communication/Types.hs
@@ -0,0 +1,17 @@
+
+-- | Types used for communication.
+
+module Pos.Communication.Types
+       ( -- * Messages and socket state
+         module Pos.Communication.Types.Protocol
+       , module Pos.Communication.Types.Relay
+       , module Pos.Communication.Types.State
+       , module Pos.Communication.Types.SysStart
+       , module Pos.Txp.Network.Types
+       ) where
+
+import           Pos.Communication.Types.Protocol
+import           Pos.Communication.Types.Relay
+import           Pos.Communication.Types.State
+import           Pos.Communication.Types.SysStart
+import           Pos.Txp.Network.Types
