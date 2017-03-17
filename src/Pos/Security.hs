be58f406bcc348df5656fe3a86f00a49be8d3067
Chris Done <chrisdone@gmail.com>
2017-03-17 12:42:55 +0000

    src/Pos/Security

---
+++ src/Pos/Security.hs
@@ -0,0 +1,9 @@
+module Pos.Security
+       ( module Pos.Security.Class
+       , module Pos.Security.CLI
+       , module Pos.Security.Util
+       ) where
+
+import           Pos.Security.Class
+import           Pos.Security.CLI
+import           Pos.Security.Util
---
+++ src/Pos/Security/Class.hs
@@ -0,0 +1,12 @@
+module Pos.Security.Class
+       ( SecurityWorkersClass (..)
+       ) where
+
+import           Data.Tagged                (Tagged)
+
+import           Pos.Communication.Protocol (OutSpecs, WorkerSpec)
+import           Pos.Ssc.Class.Types        (Ssc)
+import           Pos.WorkMode               (WorkMode)
+
+class Ssc ssc => SecurityWorkersClass ssc where
+    securityWorkers :: WorkMode ssc m => Tagged ssc ([WorkerSpec m], OutSpecs)
